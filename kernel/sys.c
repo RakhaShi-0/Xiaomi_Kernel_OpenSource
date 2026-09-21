83c40ded706b525670ef05538c237c90af7b88e2
RakhaShi-0 <rakhashii211@gmail.com>
2026-09-21 21:30:40 +0700

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1271,7 +1271,9 @@ static int __init uname_spoof_setup(char *s)
 	}
 	return 1;
 }
-__setup("uname_spoof=", uname_spoof_setup);
+static const char * const uname_spoof_targets[] = {
+   	"",		/* kosong = spoof mati. Isi nama proses di sini */
+   };
 
 bool uname_should_spoof(void)
 {
