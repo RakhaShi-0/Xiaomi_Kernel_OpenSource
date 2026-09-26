9f15669e0de2e93fc14d6e3f7e6cdd87e928958d
RakhaShi-0 <rakhashii211@gmail.com>
2026-09-26 22:07:07 +0700

    Update bpf.h

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -127,6 +127,7 @@ enum bpf_map_type {
 	BPF_MAP_TYPE_SOCKHASH,
 	BPF_MAP_TYPE_CGROUP_STORAGE,
 	BPF_MAP_TYPE_REUSEPORT_SOCKARRAY,
+	BPF_MAP_TYPE_RINGBUF,
 };
 
 enum bpf_prog_type {
@@ -2280,7 +2281,12 @@ union bpf_attr {
 	FN(get_netns_cookie),		\
 	FN(get_current_ancestor_cgroup_id),	\
 	FN(sk_assign),			\
-	FN(ktime_get_boot_ns),
+	FN(ktime_get_boot_ns),		\
+	FN(ringbuf_output),		\
+	FN(ringbuf_reserve),		\
+	FN(ringbuf_submit),		\
+	FN(ringbuf_discard),		\
+	FN(ringbuf_query),
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
  * function eBPF program intends to call
@@ -2335,6 +2341,15 @@ enum bpf_func_id {
 #define BPF_F_CURRENT_CPU		BPF_F_INDEX_MASK
 /* BPF_FUNC_perf_event_output for sk_buff input context. */
 #define BPF_F_CTXLEN_MASK		(0xfffffULL << 32)
+/* BPF_FUNC_ringbuf_output/reserve/submit/discard flags. */
+#define BPF_RB_NO_WAKEUP		(1ULL << 0)
+#define BPF_RB_FORCE_WAKEUP		(1ULL << 1)
+
+/* BPF_FUNC_ringbuf_query flags */
+#define BPF_RB_AVAIL_DATA	0
+#define BPF_RB_RING_SIZE	1
+#define BPF_RB_CONS_POS		2
+#define BPF_RB_PROD_POS		3
 
 /* Mode for BPF_FUNC_skb_adjust_room helper. */
 enum bpf_adj_room_mode {
